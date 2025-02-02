4d424f8094d5ccfff97f9c88e225310eada94f00
rolandoandre123 <rbardalespriale@gmail.com>
2025-02-02 17:52:01 -0500

    add : implement toolbar on the sidebar

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HeroesRoutingModule } from "./heroes-routing.module";
+import { MaterialModule } from "../material/material.module";
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
-import { HeroesRoutingModule } from "./heroes-routing.module";
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { HeroesRoutingModule } from "./heroes-routing.module";
     SearchPageComponent
   ],
   imports: [
+    CommonModule,
     HeroesRoutingModule,
+    MaterialModule,
   ],
   exports: [],
   providers: [],
