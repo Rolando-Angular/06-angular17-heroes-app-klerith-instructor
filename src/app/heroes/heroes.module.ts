fa81c653c580c342a7c49445d0bfd93665a5cdcb
rolandoandre123 <rbardalespriale@gmail.com>
2025-02-02 23:04:45 -0500

    add : creating custom pipe for heroe image url

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -8,6 +8,7 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,8 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     ListPageComponent,
     NewPageComponent,
     SearchPageComponent,
+    // Pipes
+    HeroImagePipe,
   ],
   imports: [
     CommonModule,
